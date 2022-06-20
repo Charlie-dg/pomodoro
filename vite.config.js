848e985c13a4d403567ffbe8d8d150b00b5c5950
Charlie-dg <kilker.9527012228@gmail.com>
2022-06-20 12:13:46 +0800

    resave vit.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,13 +9,13 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-		vue(),
-		vuetify({ autoImport: true }),
+    vue(),
+    vuetify({ autoImport: true }),
     ViteRadar({
       // Google Analytics tag injection
       analytics: {
-        id: 'G-MCTEYFG67E',
-      },
+        id: 'G-MCTEYFG67E'
+      }
     }),
     VitePWA({
       registerType: 'autoUpdate',
@@ -36,83 +36,83 @@ export default defineConfig({
         start_url: './',
         display: 'standalone',
         // icon 路徑，./ 代表 public
-        "icons": [
+        icons: [
           {
-            "src": "./img/icons/android-chrome-192x192.png",
-            "sizes": "192x192",
-            "type": "image/png"
+            src: './img/icons/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
           },
           {
-            "src": "./img/icons/android-chrome-512x512.png",
-            "sizes": "512x512",
-            "type": "image/png"
+            src: './img/icons/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
           },
           {
-            "src": "./img/icons/android-chrome-maskable-192x192.png",
-            "sizes": "192x192",
-            "type": "image/png",
-            "purpose": "maskable"
+            src: './img/icons/android-chrome-maskable-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable'
           },
           {
-            "src": "./img/icons/android-chrome-maskable-512x512.png",
-            "sizes": "512x512",
-            "type": "image/png",
-            "purpose": "maskable"
+            src: './img/icons/android-chrome-maskable-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
           },
           {
-            "src": "./img/icons/apple-touch-icon-60x60.png",
-            "sizes": "60x60",
-            "type": "image/png"
+            src: './img/icons/apple-touch-icon-60x60.png',
+            sizes: '60x60',
+            type: 'image/png'
           },
           {
-            "src": "./img/icons/apple-touch-icon-76x76.png",
-            "sizes": "76x76",
-            "type": "image/png"
+            src: './img/icons/apple-touch-icon-76x76.png',
+            sizes: '76x76',
+            type: 'image/png'
           },
           {
-            "src": "./img/icons/apple-touch-icon-120x120.png",
-            "sizes": "120x120",
-            "type": "image/png"
+            src: './img/icons/apple-touch-icon-120x120.png',
+            sizes: '120x120',
+            type: 'image/png'
           },
           {
-            "src": "./img/icons/apple-touch-icon-152x152.png",
-            "sizes": "152x152",
-            "type": "image/png"
+            src: './img/icons/apple-touch-icon-152x152.png',
+            sizes: '152x152',
+            type: 'image/png'
           },
           {
-            "src": "./img/icons/apple-touch-icon-180x180.png",
-            "sizes": "180x180",
-            "type": "image/png"
+            src: './img/icons/apple-touch-icon-180x180.png',
+            sizes: '180x180',
+            type: 'image/png'
           },
           {
-            "src": "./img/icons/apple-touch-icon.png",
-            "sizes": "180x180",
-            "type": "image/png"
+            src: './img/icons/apple-touch-icon.png',
+            sizes: '180x180',
+            type: 'image/png'
           },
           {
-            "src": "./img/icons/favicon-16x16.png",
-            "sizes": "16x16",
-            "type": "image/png"
+            src: './img/icons/favicon-16x16.png',
+            sizes: '16x16',
+            type: 'image/png'
           },
           {
-            "src": "./img/icons/favicon-32x32.png",
-            "sizes": "32x32",
-            "type": "image/png"
+            src: './img/icons/favicon-32x32.png',
+            sizes: '32x32',
+            type: 'image/png'
           },
           {
-            "src": "./img/icons/msapplication-icon-144x144.png",
-            "sizes": "144x144",
-            "type": "image/png"
+            src: './img/icons/msapplication-icon-144x144.png',
+            sizes: '144x144',
+            type: 'image/png'
           },
           {
-            "src": "./img/icons/mstile-150x150.png",
-            "sizes": "150x150",
-            "type": "image/png"
+            src: './img/icons/mstile-150x150.png',
+            sizes: '150x150',
+            type: 'image/png'
           }
         ]
       }
     })
-],
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
